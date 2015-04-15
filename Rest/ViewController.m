786b20de38bf3541079c08c2c37b460606f09ffa
tannerd <tannerd@mail.gvsu.edu>
2015-04-15 16:12:36 -0400

    https

--- Rest/ViewController.m
+++ Rest/ViewController.m
@@ -22,45 +22,16 @@
 @implementation ViewController
 
 
-- (BOOL)connection:(NSURLConnection *)connection
-canAuthenticateAgainstProtectionSpace:(NSURLProtectionSpace *)protectionSpace
-{
-    return [protectionSpace.authenticationMethod
-            isEqualToString:NSURLAuthenticationMethodServerTrust];
-}
 
-- (void)connection:(NSURLConnection *)connection
-didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge
-{
-    if ([challenge.protectionSpace.authenticationMethod
-         isEqualToString:NSURLAuthenticationMethodServerTrust])
-    {
-        // we only trust our own domain
-        if ([challenge.protectionSpace.host isEqualToString:@"https://discoverservice.bluesphereqa.com/"])
-        {
-            NSURLCredential *credential =
-            [NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust];
-            [challenge.sender useCredential:credential forAuthenticationChallenge:challenge];
-        }
-    }
-    
-    [challenge.sender continueWithoutCredentialForAuthenticationChallenge:challenge];
-}
-
-
-
-- (IBAction)getContact;
-{
- 
-    
-    
-    NSURL *url = [NSURL URLWithString:@"https://discoverservice.bluesphereqa.com/api/testcontact"];
+- (IBAction)getContacts{
+    NSURL *url = [NSURL URLWithString:@"https://discoverservice.bluesphereqa.com/api/contact"];
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
     [request setValue:@"application/xml" forHTTPHeaderField:@"Content-Type"];
     [request setValue:@"application/xml" forHTTPHeaderField:@"Accept"];
-    NSURLConnection *connect = [NSURLConnection connectionWithRequest:request delegate:self];
+    NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+    [connection start];
     
-//    [self connection:connect canAuthenticateAgainstProtectionSpace:];
+    
     [NSURLConnection sendAsynchronousRequest:request
                                        queue:[NSOperationQueue mainQueue]
                            completionHandler:^(NSURLResponse *response,
@@ -69,26 +40,55 @@ didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge
          if (data.length > 0 && connectionError == nil)
          {
              NSString *string = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-             NSLog(@"%@",string);
+             
+             NSLog(string);
+             
          }
      }];
+  
+
+
+    
 }
+
+- (BOOL)connection:(NSURLConnection *)connection canAuthenticateAgainstProtectionSpace:(NSURLProtectionSpace *)protectionSpace {
+    return [protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge {
+    if([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust]){
+        [challenge.sender useCredential:[NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust] forAuthenticationChallenge:challenge];
+    }
+}
+
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection{
+    NSLog(@"end");
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error{
+    NSLog(@"%@", error.localizedDescription);
+}
+
 
 - (IBAction)updateContact;
 {
     //Cathan to DavidTanner
-    NSString *entity =  @"<EntityCollection xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://schemas.microsoft.com/xrm/2011/Contracts\"><Entities><Entity><Attributes xmlns:d4p1=\"http://schemas.datacontract.org/2004/07/System.Collections.Generic\"><KeyValuePairOfstringanyType><d4p1:key>contactid</d4p1:key><d4p1:value xmlns:d6p1=\"http://schemas.microsoft.com/2003/10/Serialization/\" i:type=\"d6p1:guid\">47a0e5b9-88df-e311-b8e5-6c3be5a8b200</d4p1:value></KeyValuePairOfstringanyType><KeyValuePairOfstringanyType><d4p1:key>firstname</d4p1:key><d4p1:value xmlns:d6p1=\"http://www.w3.org/2001/XMLSchema\" i:type=\"d6p1:string\">DavidTanner</d4p1:value></KeyValuePairOfstringanyType><KeyValuePairOfstringanyType><d4p1:key>lastname</d4p1:key><d4p1:value xmlns:d6p1=\"http://www.w3.org/2001/XMLSchema\" i:type=\"d6p1:string\">Lauriant</d4p1:value></KeyValuePairOfstringanyType><KeyValuePairOfstringanyType><d4p1:key>emailaddress1</d4p1:key><d4p1:value xmlns:d6p1=\"http://www.w3.org/2001/XMLSchema\" i:type=\"d6p1:string\">vlauriant@adatum.com</d4p1:value></KeyValuePairOfstringanyType></Attributes><EntityState i:nil=\"true\" /><FormattedValues xmlns:d4p1=\"http://schemas.datacontract.org/2004/07/System.Collections.Generic\" /><Id>47a0e5b9-88df-e311-b8e5-6c3be5a8b200</Id><LogicalName>contact</LogicalName><RelatedEntities xmlns:d4p1=\"http://schemas.datacontract.org/2004/07/System.Collections.Generic\" /></Entity></Entities><EntityName>contact</EntityName><MinActiveRowVersion>-1</MinActiveRowVersion><MoreRecords>false</MoreRecords><PagingCookie>&lt;cookie page=\"1\"&gt;&lt;contactid last=\"{E5A7E5B9-88DF-E311-B8E5-6C3BE5A8B200}\" first=\"{47A0E5B9-88DF-E311-B8E5-6C3BE5A8B200}\" /&gt;&lt;/cookie&gt;</PagingCookie><TotalRecordCount>-1</TotalRecordCount><TotalRecordCountLimitExceeded>false</TotalRecordCountLimitExceeded></EntityCollection>";
+    NSString *entity =  @"<EntityCollection xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://schemas.microsoft.com/xrm/2011/Contracts\"><Entities><Entity><Attributes xmlns:d4p1=\"http://schemas.datacontract.org/2004/07/System.Collections.Generic\"><KeyValuePairOfstringanyType><d4p1:key>contactid</d4p1:key><d4p1:value xmlns:d6p1=\"http://schemas.microsoft.com/2003/10/Serialization/\" i:type=\"d6p1:guid\">47a0e5b9-88df-e311-b8e5-6c3be5a8b200</d4p1:value></KeyValuePairOfstringanyType><KeyValuePairOfstringanyType><d4p1:key>firstname</d4p1:key><d4p1:value xmlns:d6p1=\"http://www.w3.org/2001/XMLSchema\" i:type=\"d6p1:string\">TTTTTanner</d4p1:value></KeyValuePairOfstringanyType><KeyValuePairOfstringanyType><d4p1:key>lastname</d4p1:key><d4p1:value xmlns:d6p1=\"http://www.w3.org/2001/XMLSchema\" i:type=\"d6p1:string\">Lauriant</d4p1:value></KeyValuePairOfstringanyType><KeyValuePairOfstringanyType><d4p1:key>emailaddress1</d4p1:key><d4p1:value xmlns:d6p1=\"http://www.w3.org/2001/XMLSchema\" i:type=\"d6p1:string\">vlauriant@adatum.com</d4p1:value></KeyValuePairOfstringanyType></Attributes><EntityState i:nil=\"true\" /><FormattedValues xmlns:d4p1=\"http://schemas.datacontract.org/2004/07/System.Collections.Generic\" /><Id>47a0e5b9-88df-e311-b8e5-6c3be5a8b200</Id><LogicalName>contact</LogicalName><RelatedEntities xmlns:d4p1=\"http://schemas.datacontract.org/2004/07/System.Collections.Generic\" /></Entity></Entities><EntityName>contact</EntityName><MinActiveRowVersion>-1</MinActiveRowVersion><MoreRecords>false</MoreRecords><PagingCookie>&lt;cookie page=\"1\"&gt;&lt;contactid last=\"{E5A7E5B9-88DF-E311-B8E5-6C3BE5A8B200}\" first=\"{47A0E5B9-88DF-E311-B8E5-6C3BE5A8B200}\" /&gt;&lt;/cookie&gt;</PagingCookie><TotalRecordCount>-1</TotalRecordCount><TotalRecordCountLimitExceeded>false</TotalRecordCountLimitExceeded></EntityCollection>";
     
     //NSData* contact = [entity dataUsingEncoding:NSUTF8StringEncoding];
     NSData *postData = [entity dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
     NSString *postLength = [NSString stringWithFormat:@"%d",[postData length]];
-    NSURL *url = [NSURL URLWithString:@"http://discoverservice.bluesphereqa.com/api/testcontact"];
+    NSURL *url = [NSURL URLWithString:@"https://discoverservice.bluesphereqa.com/api/contact"];
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
     [request setHTTPMethod:@"PUT"];
     [request setValue:@"application/xml" forHTTPHeaderField:@"Content-Type"];
     [request setValue:@"application/xml" forHTTPHeaderField:@"Accept"];
     [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
     [request setHTTPBody:postData];
+    
+    NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+    [connection start];
     
     
     
@@ -108,57 +108,56 @@ didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge
 }
 //http://discoverservice.bluesphereqa.com/api/contact/?url=https://CDH62CommercialwithCRM.crm.dynamics.com,name=alans@CDH62CommercialwithCRM.onmicrosoft.com,password=%22Vulo5319%22
 
-/*-(BOOL)connection:(NSURLConnection *)connection canAuthenticateAgainstProtectionSpace:(NSURLProtectionSpace *)protectionSpace {
-    return [protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust];
-}
-
-- (void)connection:(NSURLConnection *)connection didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge {
-    if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust])
-        if ([trustedHosts containsObject:challenge.protectionSpace.host])
-            [challenge.sender useCredential:[NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust] forAuthenticationChallenge:challenge];
-    
-    [challenge.sender continueWithoutCredentialForAuthenticationChallenge:challenge];
-}*/
-
 
 - (IBAction)logIn;
 {
-    
     //NSData* contact = [entity dataUsingEncoding:NSUTF8StringEncoding];
     
-    NSURL *url = [NSURL URLWithString:@"http://discoverservice.bluesphereqa.com/api/contact"];
+    NSURL *url = [NSURL URLWithString:@"https://discoverservice.bluesphereqa.com/api/contact"];
     NSString *discoverurl = @"https://CDH62CommercialwithCRM.crm.dynamics.com";
     NSString *name = @"alans@CDH62CommercialwithCRM.onmicrosoft.com";
     NSString *password = @"Vulo5319";
-    NSString *xmlFormat = [NSString stringWithFormat:@"<CreateUserLogIn xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://schemas.datacontract.org/2004/07/DiscoveryServiceWebAPI.Controllers\"><name>%@</name><password>%@</password><url>%@</url></CreateUserLogIn>",name,password,discoverurl];
+    
+ /*   "<CreateUserLogIn xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://schemas.datacontract.org/2004/07/DiscoveryServiceWebAPI.Controllers\"><name>alans@CDH62CommercialwithCRM.onmicrosoft.com</name><password>Vulo5319</password><url>https://CDH62CommercialwithCRM.crm.dynamics.com</url></CreateUserLogIn>";*/
     
     
-    //NSString *post = [NSString stringWithFormat:@"url=%@,name=%@,password=%@",discoverurl,name,password];
+NSString *xmlFormat = [NSString stringWithFormat:@"<CreateUserLogIn xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://schemas.datacontract.org/2004/07/DiscoveryServiceWebAPI.Controllers\"><name>%@</name><password>%@</password><url>%@</url></CreateUserLogIn>",name,password,discoverurl];
+    
     NSData *postData = [xmlFormat dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
     [request setHTTPMethod:@"POST"];
     [request setValue:@"application/xml" forHTTPHeaderField:@"Content-Type"];
     [request setValue:@"application/xml" forHTTPHeaderField:@"Accept"];
     [request setHTTPBody:postData];
-    NSHTTPURLResponse __autoreleasing *response = nil;
-    NSError __autoreleasing *error = nil;
-    NSData *result = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
-    if (response){
-        NSString *string = [[NSString alloc] initWithData:result encoding:NSUTF8StringEncoding];
-        NSLog(@"%@",string);
-    }
-    if (error){
-        // code to handle with the error
-        NSLog(@"There was an error. Please check your username and password");
-    }
+    
+    NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+    [connection start];
+    
+    NSData *returnData = [[NSData alloc]init];
+    returnData = [NSURLConnection sendSynchronousRequest: request returningResponse: nil error: nil];
+    
+    
+    [NSURLConnection sendAsynchronousRequest:request
+                                       queue:[NSOperationQueue mainQueue]
+                           completionHandler:^(NSURLResponse *response,
+                                               NSData *data, NSError *connectionError)
+     
+     {
+         if (data.length > 0 && connectionError == nil)
+         {
+             NSString *string = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+             NSLog(@"%@",string);
+         }
+     }];
+
     
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     //[self updateContact];
-    //[self logIn];
-    [self getContact];
+    [self logIn];
+    //[self getConnection];
     //[self updateContact];
     // Do any additional setup after loading the view, typically from a nib.
 }
